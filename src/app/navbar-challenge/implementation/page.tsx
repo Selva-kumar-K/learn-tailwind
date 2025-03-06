60f9061d4b2fe34b0d8028bb0da91e42701c1524
Selva-kumar-K <selvanithish2014@gmail.com>
2025-03-06 14:37:56 +0530

    resolved errors

--- src/app/navbar-challenge/implementation/page.tsx
+++ src/app/navbar-challenge/implementation/page.tsx
@@ -4,7 +4,6 @@ import Confetti from "react-confetti";
 
 import Image from "next/image";
 import Logo from "./../../../../public/cool.png";
-import Gif from "./../../../../public/mrbean-dance.gif";
 import { navMenus } from "@/app/libs/menus";
 
 export default function Implementation() {
